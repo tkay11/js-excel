0707a691f6eccedf96d33bed97d72b8c62d9cd7e
tkay11 <tanea.23@yandex.ru>
2020-10-27 20:51:14 +0300

    less 30 completed

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,10 @@
+import {DomListener} from "@core/DomListener";
+
+export class ExcelComponent extends DomListener {
+    //Возвращает шаблон компонента
+    toHTML() {
+        return ''
+    }
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './sass/index.scss'
+import {Excel} from "@/components/excel/Excel";
 
-console.log('Working!')
+const excel = new Excel('#app', {
+    components: []
+})
+
+console.log('excel', excel)

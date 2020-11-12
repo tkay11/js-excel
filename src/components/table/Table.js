f253a9b6ea3ff35e3832698c7e62cbe22e96a3e1
tkay11 <tanea.23@yandex.ru>
2020-11-12 23:12:43 +0300

    less 49

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -23,23 +23,47 @@ export class Table extends ExcelComponent {
       // console.log('mousedown', event.target.getAttribute('data-resize'))
       if (event.target.dataset.resize) {
         const $resizer = $(event.target)
-        // const $parent = $resizer.$el.parentNode //bad!
-        // const $parent = $resizer.$el.closest('.column') //better but bad
         const $parent = $resizer.closest('[data-type="resizable"]')
         const coords = $parent.getCoords()
+        const type =$resizer.data.resize
+        const sideProp = type === 'col' ? 'bottom' : 'right'
+        let value
 
-        const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
-
+        $resizer.css({
+          opacity: 1,
+          [sideProp]: '-5000px'
+        })
 
         document.onmousemove = e => {
-          const delta = e.pageX - coords.right
-          const value = coords.width + delta
-          $parent.$el.style.width = value + 'px'
-          cells.forEach(el => el.style.width = value + 'px')
+          if (type === 'col') {
+            const delta = e.pageX - coords.right
+            value = coords.width + delta
+            $resizer.css({right: -delta + 'px'})
+          } else {
+            const delta =e.pageY - coords.bottom
+            value = coords.height + delta
+            $resizer.css({bottom: -delta +'px'})
+          }
         }
 
         document.onmouseup = () => {
           document.onmousemove = null
+          document.onmouseup = null
+
+          if (type === 'col') {
+            $parent.css({width: value + 'px'})
+            this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+                .forEach(el =>el.style.width = value + 'px')
+          } else {
+            $parent.css({height: value + 'px'})
+          }
+
+
+          $resizer.css({
+            opacity: 0,
+            bottom: 0,
+            right: 0
+          })
         }
       }
     }

6d6f8144def14f3f3e38f3f2f4a038054247ce77
tkay11 <tanea.23@yandex.ru>
2020-11-12 14:29:51 +0300

    less 38

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -18,6 +18,7 @@ export class Formula extends ExcelComponent {
     }
 
     onInput(event) {
+      console.log(this.$root)
       console.log('Formula: onInput', event.target.textContent.trim())
     }
 

98b7c175dbfd2e6c3b0dfe073c3e421a7aafe112
tkay11 <tanea.23@yandex.ru>
2020-11-12 17:30:24 +0300

    less 43

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,32 @@ import {createTable} from "@/components/table/table.template";
 export class Table extends ExcelComponent {
     static className = 'excel__table'
 
+    constructor($root) {
+      super($root, {
+        listeners: ['mousedown']
+      })
+    }
+
     toHTML() {
       return createTable(20)
     }
+
+    //     onClick() {
+    //       console.log('click')
+    //     }
+
+    onMousedown(event) {
+      // console.log('mousedown', event.target.getAttribute('data-resize'))
+      if (event.target.dataset.resize) {
+        console.log('Start resizing', event.target.dataset.resize)
+      }
+    }
+//
+//     onMousemove() {
+//       console.log('mousemove')
+//     }
+//
+//     onMouseup() {
+//       console.log('mouseup')
+//     }
 }

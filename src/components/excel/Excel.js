0f3694b57426b7ef42485dc9ba729c30745cf238
tkay11 <tanea.23@yandex.ru>
2020-12-01 16:40:04 +0300

    less 72

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,5 +1,6 @@
-import {$} from "@core/dom";
-import {Emitter} from "@core/Emitter";
+import {$} from '@core/dom'
+import {Emitter} from '@core/Emitter'
+import {StoreSubscriber} from '@core/StoreSubscriber'
 
 export class Excel {
   constructor(selector, options) {
@@ -7,6 +8,7 @@ export class Excel {
     this.components = options.components || []
     this.store = options.store
     this.emitter = new Emitter()
+    this.subscriber = new StoreSubscriber(this.store)
   }
 
   getRoot() {
@@ -20,7 +22,6 @@ export class Excel {
     this.components = this.components.map(Component => {
       const $el = $.create('div', Component.className)
       const component = new Component($el, componentOptions)
-
       $el.html(component.toHTML())
       $root.append($el)
       return component
@@ -32,10 +33,12 @@ export class Excel {
   render() {
     this.$el.append(this.getRoot())
 
+    this.subscriber.subscribeComponents(this.components)
     this.components.forEach(component => component.init())
   }
 
   destroy() {
+    this.subscriber.unsubscribeFromStore()
     this.components.forEach(component => component.destroy())
   }
 }
